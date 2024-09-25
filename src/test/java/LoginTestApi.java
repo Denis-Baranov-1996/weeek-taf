28fb7abac4bfd44aef4d5597daadfde9aa80e9f0
Denis-Baranov-1996 <denizzka96@gmail.com>
2024-09-25 19:53:38 +0300

    refactor LoginTestApi add Main

--- src/test/java/LoginTestApi.java
+++ src/test/java/LoginTestApi.java
@@ -1,9 +1,61 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+
 public class LoginTestApi {
     @Test
-            public void LoginTestApi() {
-        RestAssured.when().post("https://api.weeek.net/auth/login").then().log().all();
+    public void LoginTestApi() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().log().all();
+    }
+    @Test
+    public void LoginTestApiCheckStatusCodeInBody() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().statusCode(422);
+    }
+    @Test
+    public void LoginTestApiCheckSuccessInBody() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().statusCode(422).
+                body("success", equalTo(false));
+    }
+
+    @Test
+    public void LoginTestApiCheckIfWriteNotWordSuccessInBody() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().statusCode(422).
+                body("success_any", equalTo(false));
+    }
+
+    @Test
+    public void LoginTestApiCheckSucccessIfTrueInBody() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().log().all().
+                statusCode(422).
+                body("success_any", equalTo(true));
+    }
+
+    @Test
+    public void LoginTestApiCheckSuccessMessageEmailPassword() {
+        RestAssured.
+                when().
+                post("https://api.weeek.net/auth/login").
+                then().log().body().
+                statusCode(422).
+                body("success", equalTo(false)).
+                body("message",equalTo("The field is required. (and 1 more error)")).
+                body("errors.email[0]",equalTo("The field is required.")).
+                body("errors.password[0]",equalTo("The field is required."));
     }
 }
