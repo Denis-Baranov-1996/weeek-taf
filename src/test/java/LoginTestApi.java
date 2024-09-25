1ca8684baecef949bda9c11bf947e0d2846d0266
Denis-Baranov-1996 <denizzka96@gmail.com>
2024-09-25 17:16:04 +0300

    refactor LoginTestApi add Main

--- src/test/java/LoginTestApi.java
+++ src/test/java/LoginTestApi.java
@@ -1,3 +1,9 @@
-public class LoginTestApi {
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 
+public class LoginTestApi {
+    @Test
+            public void LoginTestApi() {
+        RestAssured.when().post("https://api.weeek.net/auth/login").then().log().all();
+    }
 }
